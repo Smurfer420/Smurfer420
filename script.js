c79bbd3cbe682a926a5cb4ed93aef701fa7c52f6
Smurfer <102403498+Smurfer420@users.noreply.github.com>
2026-07-07 21:46:44 +0200

    Update fetch URL in script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document
+.querySelector("#contact")
+.addEventListener("submit", async e => {
+
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const response = await fetch(
+        "https://smurfer420.smurfer42.workers.dev/",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: form.get("name"),
+                message: form.get("message"),
+                website: form.get("website")
+            })
+        }
+    );
+
+
+    if (response.ok) {
+        alert("Message sent!");
+    } else {
+        alert("Failed to send.");
+    }
+
+});
